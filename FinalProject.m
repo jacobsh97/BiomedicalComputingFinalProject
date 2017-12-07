1a9112182f6f2d593ae19e55d8002bce3261b8dc
jacobsh97 <jacobsh97@yahoo.com>
2017-12-07 12:13:37 -0500

    a

--- FinalProject.m
+++ FinalProject.m
@@ -1,28 +1,41 @@
 clear all;
 clc;
 
-a = arduino();
+% a = arduino();
 m = mobiledev;
 
 m.AccelerationSensorEnabled = 1;
 m.Logging = 1;
 
 pause(5);
+display('stop');
 
 m.Logging = 0;
-
+m.AccelerationSensorEnabled = 0;
 [acceleration, t] = accellog(m);
 
 plot(t,acceleration);
 legend('x','y','z');
 
+x = acceleration(:,1)';
+y = acceleration(:,2)';
+z = acceleration(:,3)';
+acc = sqrt(x.^2)+sqrt(y.^2)+sqrt(z.^2);
 
+sdsum = std(acc);
 
+figure(2);
+[peaks,locs,w,p] = findpeaks(acc,'MinPeakHeight',sdsum);
+findpeaks(acc,'MinPeakHeight',sdsum,'Annotate','extents');
 
+sum = 0;
+for(i = 1:length(p))
+    if(p(i) > std(p))
+        sum = sum+1;
+    end
+end
 
-
-
-
+sum
 % for(i = 1:10)
 %     writeDigitalPin(a,2,1);
 %     pause(1);
